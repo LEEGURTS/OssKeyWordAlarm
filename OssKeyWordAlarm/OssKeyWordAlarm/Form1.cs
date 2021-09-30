964729b396842e052132ca572eff9d5146d5d3c9
jaeseung-lee <kevin0969@naver.com>
2021-10-01 02:04:53 +0900

    [main] HTMLagility tests, README updated

--- OssKeyWordAlarm/OssKeyWordAlarm/Form1.cs
+++ OssKeyWordAlarm/OssKeyWordAlarm/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using HtmlAgilityPack;
 
 namespace OssKeyWordAlarm
 {
@@ -15,6 +17,24 @@ namespace OssKeyWordAlarm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void example()
+        {
+            var url = @"https://www.kw.ac.kr/ko/life/notice.jsp";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(url);
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+            Console.WriteLine("Node Name :" + node.Name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            example();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OssKeyWordAlarm
 2021년 2-2 오픈소스소프트웨어 9조 웅크린돼지는말이없다 조입니다.
+
+# 개발 계획서
+https://docs.google.com/document/d/1HOlnT9En_JWWfogjHc38Q5JBG4sGQ_uQO-AW9TSRHpo/edit?usp=sharing
+
+# 개발 방향 및 세부 계획
+https://docs.google.com/document/d/1e-43ALtolTDbHNK2VLFGxxBvcFB-Qg42jObMMFvVO2o/edit?usp=sharing
