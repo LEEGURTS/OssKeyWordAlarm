bcecbd9147c0301a51c0ebc69922706229fb1873
Lee.Guts <rmstjd333@gmail.com>
2021-11-13 03:08:45 +0900

    기록 적용. 기록은 Form1에서 newrecordAlar.RecordShow(List<string> 제목,List<string> url)의 형태로 이용 가능하며 제목과 url 리스트를 함수에 제공시 클릭시 링크에 접속할 수 있는 UI를 제공함.

---
+++ OssKeyWordAlarm/OssKeyWordAlarm/Forms/Notice.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OssKeyWordAlarm.Forms
+{
+
+
+    public partial class Notice : UserControl
+    {
+
+        public Notice()
+        {
+            InitializeComponent();
+        }
+        public Notice(string str)
+        {
+            lbltitle.Text = str;
+            InitializeComponent();          
+        }
+        #region Properties
+        private string _title;
+        private string _url;
+        [Category ("MyList")]
+        public string title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                lbltitle.Text = value;
+            }
+        }
+        [Category("MyList")]
+        public string url {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                _url = value;
+                lblLink.Text = value;
+            }
+        }
+
+        #endregion
+
+        private void Notice_MouseClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start(_url);
+        }
+    }
+}
